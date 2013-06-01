5d9a915a3af5cb63275d316a30ecdb1f00ec9d3f
Hrafn Eiríksson <hrafne@gmail.com>
2013-06-01 13:23:01 +0000

    A products table and user authentication system with a little bit of help from Devise.

---
+++ web/app/models/product.rb
@@ -0,0 +1,3 @@
+class Product < ActiveRecord::Base
+  attr_accessible :is_per_kg, :name
+end
--- web/config/environments/development.rb
+++ web/config/environments/development.rb
@@ -34,4 +34,7 @@ Web::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Something Devise told me to do.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
